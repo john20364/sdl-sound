6fb5bbc8e0c01b0467881b55db9dabd28340c13a
john20364 <j.boelee@gmail.com>
2016-07-18 23:51:32 +0200

    first try to generate sound

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <string.h>
 
 using namespace std;
 
@@ -10,21 +11,50 @@ void SDLAudioCallback(
 }
 
 int main(int argc, char **argv) {
-	int SamplesPerSecond = 48000;
-	int ToneHz = 256;
-	int ToneVolume = 3000;
-	int SquareWavePeriod = SamplesPerSecond / ToneHz;
-	Uint32 RunningSampleIndex = 0;
-	Uint32 BufferSize = 4096;
+	int SamplesPerSecond = 48000; // the sampling frequency of our audio device.
+	int ToneHz = 256; // The frequency of the square wave.
+	int ToneVolume = 3000; // The volume of our square wave: 32767 (int16) is loudest, 0 is silent.
+	int SquareWavePeriod = SamplesPerSecond / ToneHz; // The period of our squarewave in samples.
+	int HalfSquareWavePeriod = SquareWavePeriod / 2; // The number of samples for which the square wave 
+													 // is held high or low.
+	int BytesPerSample = sizeof(int16_t) * 2; // The number of bytes per stereo sample. (one sample for left
+											  // one sample for right).
+
+	Uint32 RunningSampleIndex = 0; // The index of the next sample we are going to generate. We need to 
+	                               // preserve this across writes to our buffer, else the wave mightn't
+								   // be consistent across write boundaries.
+
+	// If out game is running at 60 frames per second, that's 800 samples (at 48000 Hz) per frame.
+//	int BytesToWrite = 800 * BytesPerSample;
+	int BytesToWrite = 48000 * BytesPerSample;
+
+//	Uint32 BufferSize = 4096;
 
 	SDL_AudioSpec AudioSettings = {0};
 	AudioSettings.freq = SamplesPerSecond;
 	AudioSettings.format = AUDIO_S16LSB;
 	AudioSettings.channels = 2;
-	AudioSettings.samples = BufferSize;
-	AudioSettings.callback = SDLAudioCallback;
+//	AudioSettings.samples = BufferSize / 2;
+	//AudioSettings.callback = SDLAudioCallback;
+	AudioSettings.callback = NULL;
+
+	void *SoundBuffer = malloc(BytesToWrite);
+	int16_t *SampleOut = (int16_t *)SoundBuffer;
+	int SampleCount = BytesToWrite / BytesPerSample;
+
+	for (int SampleIndex = 0; SampleIndex < SampleCount; ++SampleIndex) {
+		int16_t SampleValue = ((RunningSampleIndex++ / HalfSquareWavePeriod) % 2) ? ToneVolume : -ToneVolume;
+		*SampleOut++ = SampleValue;
+		*SampleOut++ = SampleValue;
+	}
 
 	SDL_OpenAudio(&AudioSettings, NULL);
+
+	cout << "samples: " << AudioSettings.samples << endl;
+
+	SDL_QueueAudio(1, SoundBuffer, BytesToWrite);
+
+	free(SoundBuffer);
 
 	if (AudioSettings.format != AUDIO_S16LSB) {
 		cout << "Didn't get AUDIO_S16LSB" << endl;
--- makefile
+++ makefile
@@ -1,6 +1,21 @@
 CXX = g++
-SDL_LIB = -LC:/SDL/SDL2-2.0.4-mingw/x86_64-w64-mingw32/lib -lSDL2main -lSDL2 
-SDL_INCLUDE = -IC:/SDL/SDL2-2.0.4-mingw/x86_64-w64-mingw32/include 
+###############################################################################
+#
+# Windows 64
+#
+###############################################################################
+# SDL_LIB = -LC:/SDL/SDL2-2.0.4-mingw/x86_64-w64-mingw32/lib -lSDL2main -lSDL2 
+# SDL_INCLUDE = -IC:/SDL/SDL2-2.0.4-mingw/x86_64-w64-mingw32/include 
+
+###############################################################################
+#
+# Windows 32
+#
+###############################################################################
+SDL_LIB = -LC:/SDL/SDL2-2.0.4-mingw/i686-w64-mingw32/lib -lSDL2main -lSDL2 
+SDL_INCLUDE = -IC:/SDL/SDL2-2.0.4-mingw/i686-w64-mingw32/include 
+	
+
 CFLAGS = -Wall -c -std=c++11 $(SDL_INCLUDE) 
 LDFLAGS = -lmingw32 -mwindows -mconsole $(SDL_LIB) 
 OBJS = main.o 
